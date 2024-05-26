b5dd4d9e634387986a29fc47f6bbabbcb9dddb14
Sebastian Grosfeld <sebastian.grosfeld@wp.pl>
2024-05-26 20:33:06 +0200

    changes in doc and initial com

---
+++ med_registration_server/src/main/java/com/example/med_registration_server/MedRegistrationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.med_registration_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedRegistrationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedRegistrationServerApplication.class, args);
+	}
+
+}
---
+++ med_registration_server/src/test/java/com/example/med_registration_server/MedRegistrationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.med_registration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedRegistrationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
